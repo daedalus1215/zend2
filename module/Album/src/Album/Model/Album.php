aaa407b830e75cf54507ad3b37bf35da226cdab6
ladams1776 <ladams1776@yahoo.com>
2016-05-23 20:26:58 -0400

    Built up our AlbumTable that will use a TableGateway to interact with the database

--- module/Album/src/Album/Model/Album.php
+++ module/Album/src/Album/Model/Album.php
@@ -8,6 +8,13 @@ class Album
   public $artist;
   public $title;
   
+  /**
+   * Our Album entity object is a simple PHP class. In order to work with 
+   * Zend\Db’s TableGateway class, we need to implement the exchangeArray() method. 
+   * 
+   * This method simply copies the data from the passed in array to our entity’s 
+   * properties. We will add an input filter for use with our form later.
+   */
   public function exchangeArray($data) 
   {
     $this->id     = (!empty($data['id'])) ? $data['id'] : null;
