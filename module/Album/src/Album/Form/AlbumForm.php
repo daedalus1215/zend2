d5fcc985f8d5787fa4b7bb7d8814f5ef6170d9ae
ladams1776 <ladams1776@yahoo.com>
2016-05-24 20:16:54 -0400

    AlbumForm is now created, to create new Albums

--- module/Album/src/Album/Form/AlbumForm.php
+++ module/Album/src/Album/Form/AlbumForm.php
@@ -7,5 +7,35 @@ use Zend\Form\Form;
 
 class AlbumForm extends Form
 {
-  
+  public function __construct($name = null) 
+  {
+    parent::__construct('album');
+    
+    $this->add(array(
+      'name' => 'id',
+      'type' => 'Hidden',
+    ));
+    $this->add(array(
+      'name' => 'artist',
+      'type' => 'Text',
+      'options' => array(
+        'label' => 'Artist',
+      ),
+    ));
+     $this->add(array(
+      'name' => 'title',
+      'type' => 'Text',
+      'options' => array(
+        'label' => 'Title',
+      ),
+    ));
+    $this->add(array(
+        'name' => 'submit',
+        'attributes' => array(
+          'type'  => 'submit',
+          'value' => 'Go',
+          'id'    => 'submitbutton',
+        ),
+    ));
+  }
 }
