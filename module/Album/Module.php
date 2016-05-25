d6989b6eb5b94351bbb0d9be608f3f0d3166b96e
larry <ladams1776@yahoo.com>
2016-05-25 14:16:24 -0400

    d

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -34,7 +34,7 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface
    */
   public function getAutoloaderConfig() 
   {
-    //$config 
+    //$config d
 //    return array(
 //      'Zend\Loader\ClassMapAutoloader' => array(__DIR__ . '/autoload_classmap.php'),
 //      'Zend\Loader\StandardAutoloader' => array('namespaces' => array(__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
