98a743e119cee5b5e3c26a3bf6be0f0cb9c415c2
ladams1776 <ladams1776@yahoo.com>
2016-05-24 23:43:01 -0400

    Adjusted Album so it implemented a InputFilterAwareInterface withe dependency injection of InputFilterInterface

--- module/Album/src/Album/Model/Album.php
+++ module/Album/src/Album/Model/Album.php
@@ -2,11 +2,17 @@
 
 namespace Album\Model;
 
-class Album 
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface;
+
+
+class Album implements InputFilterAwareInterface
 {
   public $id;
   public $artist;
   public $title;
+  protected $inputFilter;
   
   /**
    * Our Album entity object is a simple PHP class. In order to work with 
@@ -21,5 +27,65 @@ class Album
     $this->artist = (!empty($data['artist'])) ? $data['artist'] : null;
     $this->title  = (!empty($data['title'])) ? $data['title'] : null;
   }
-  
+
+  public function getInputFilter() 
+  {
+    if (!$this->inputFilter) {
+      $inputFilter = new InputFilter();
+      
+      $inputFilter->add(array(
+        'name' => 'id',
+        'required' => true,
+        'filters' => array(
+          array('name' => 'Int'),
+        ),  
+      ));
+      
+      $inputFilter->add(array(
+        'name' => 'artist', 
+        'required' => true,
+        'filters' => array(
+          array('name' => 'StripTags'),
+          array('name' => 'StringTrim'),
+        ),
+        'validators' => array(
+          array(
+            'name' => 'StringLength', 
+            'options' => array(
+              'encoding' => 'UTF-8',
+              'min' => 1,
+              'max' => 100,
+            ),
+          ),
+        ),
+      ));
+      
+      $inputFilter->add(array(
+        'name' => 'title',
+        'required' => true,
+        'filters' => array(
+          array('name' => 'StringTags'),
+          array('name' => 'StringTrim'),
+        ),
+        'validators' => array(
+          array(
+            'name' => 'StringLength',
+            'options' => array(
+              'encoding' => 'UTF-8',
+              'min' => 1,
+              'max' => 100,
+            ),
+          ),
+        ),
+      ));
+      
+      $this->inputFilter = $inputFilter;
+    }
+    return $this->inputFilter;
+  }
+
+  public function setInputFilter(InputFilterInterface $inputFilter) 
+  {
+    throw new \Exception("Not used, please dont instantiate");
+  } 
 }
